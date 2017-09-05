e56170d4dc35cfcd1bd2adcd291d7c12afa332c1
vladotaseski <taseski.vlado@gmail.com>
2017-09-05 12:37:11 +0200

    add municipality

--- app/src/main/java/com/roomer/activities/AddRoommateActivity.java
+++ app/src/main/java/com/roomer/activities/AddRoommateActivity.java
@@ -14,7 +14,9 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 import com.roomer.activities.R;
+import com.roomer.data.Data;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,12 +24,14 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class AddRoommateActivity extends AppCompatActivity {
 
@@ -84,6 +88,8 @@ public class AddRoommateActivity extends AppCompatActivity {
         Spinner separateRoomSpinner = (Spinner) findViewById(R.id.spnSeparateRoom);
         Spinner fixPrice = (Spinner) findViewById(R.id.spnFixPrice); */
 
+
+
         ArrayAdapter<CharSequence> sexAdapter = ArrayAdapter.createFromResource(this,
                 R.array.sex, android.R.layout.simple_spinner_item);
         sexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -100,16 +106,16 @@ public class AddRoommateActivity extends AppCompatActivity {
                 R.array.categories, android.R.layout.simple_spinner_item);
         threeChoiceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<CharSequence> municipalitiesAdapter = ArrayAdapter.createFromResource(this,
-                R.array.municipalities, android.R.layout.simple_spinner_item);
-        threeChoiceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        ArrayAdapter<CharSequence> municipalitiesAdapter = ArrayAdapter.createFromResource(this,
+//                R.array.municipalities, android.R.layout.simple_spinner_item);
+//        threeChoiceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         mSex.setAdapter(sexAdapter);
         mSearchSex.setAdapter(roommateSexAdapter);
         mSeparateRoom.setAdapter(threeChoiceAdapter);
         mFixedPrice.setAdapter(threeChoiceAdapter);
         mCategories.setAdapter(categoriesAdapter);
-        mMunicipalities.setAdapter(municipalitiesAdapter);
+//        mMunicipalities.setAdapter(municipalitiesAdapter);
 
 
 
@@ -125,106 +131,119 @@ public class AddRoommateActivity extends AppCompatActivity {
     public void addNewRoommate() {
         String title = mTitle.getText().toString();
         String description = mDescription.getText().toString();
-        String years = mYears.getText().toString();
-        String searchYears = mSearchYears.getText().toString();
+        int years = Integer.parseInt(mYears.getText().toString());
+        int searchYears = Integer.parseInt(mSearchYears.getText().toString());
         String facebook = mFacebook.getText().toString();
         String phone = mPhone.getText().toString();
-        String priceFrom = mPriceFrom.getText().toString();
-        String priceTo = mPriceTo.getText().toString();
-        String m2From = mM2From.getText().toString();
-        String m2To = mM2To.getText().toString();
+        int priceFrom = Integer.parseInt(mPriceFrom.getText().toString());
+        int priceTo = Integer.parseInt(mPriceTo.getText().toString());
+        int m2From = Integer.parseInt(mM2From.getText().toString());
+        int m2To = Integer.parseInt(mM2To.getText().toString());
         String sex = mSex.getSelectedItem().toString();
         String searchSex = mSearchSex.getSelectedItem().toString();
-        String fixedPrice = mFixedPrice.getSelectedItem().toString();
-        String separateRoom = mSeparateRoom.getSelectedItem().toString();
+        boolean fixedPrice = mFixedPrice.getSelectedItem().toString().contains("Да") ? true : false;
+        boolean separateRoom = mSeparateRoom.getSelectedItem().toString().contains("Да") ? true : false;
         String categories = mCategories.getSelectedItem().toString();
         String municipalities = mMunicipalities.getSelectedItem().toString();
 
 
-        Log.i("all data", title + " " + description + " " + years + " " + searchSex + " " + searchYears + " " + facebook + " " + phone + " " +
+        String municipalityCode = getResources().getStringArray(R.array.municipalitiesValues)[mMunicipalities.getSelectedItemPosition()];
+
+        Log.i("all data ", municipalityCode + " " + description + " " + years + " " + searchSex + " " + searchYears + " " + facebook + " " + phone + " " +
                 sex + " " + priceFrom + " " + priceTo + " " + m2From + " " + m2To + " " + fixedPrice + " " + separateRoom + " ");
 
 
-        UploadRoommate gaa = new UploadRoommate(title);
-        gaa.execute();
+        UploadRoommate addNewRoommate = new UploadRoommate(title, description, years, searchYears, facebook, phone, priceFrom, priceTo, m2From, m2To,
+                sex, searchSex, fixedPrice, separateRoom, categories, municipalities, municipalityCode);
+        addNewRoommate.execute();
     }
 
     public class UploadRoommate extends AsyncTask<Void, Void, String> {
 
+
         String title;
         String description;
-        String years;
-        String searchYears;
+        int years;
+        int searchYears;
         String facebook;
         String phone;
-        String priceFrom;
-        String priceTo;
-        String m2From;
-        String m2To;
+        int priceFrom;
+        int priceTo;
+        int m2From;
+        int m2To;
         String sex;
         String searchSex;
-        String fixedPrice;
-        String separateRoom;
+        boolean fixedPrice;
+        boolean separateRoom;
         String categories;
         String municipalities;
+        String municipalityCode;
 
-        public UploadRoommate(String title) {
-//                , String description, String years, String searchYears, String facebook, String phone,
-//                              String priceFrom, String priceTo, String m2From, String m2To, String sex, String searchSex, String fixedPrice,
-//                              String separateRoom, String categories, String municipalties) {
+        public UploadRoommate(String title, String description, int years, int searchYears, String facebook, String phone,
+                              int priceFrom, int priceTo, int m2From, int m2To, String sex, String searchSex, boolean fixedPrice,
+                              boolean separateRoom, String categories, String municipalties, String municipalityCode) {
             this.title = title;
-//            this.description = description;
-//            this.years = years;
-//            this.searchYears = searchYears;
-//            this.facebook = facebook;
-//            this.phone = phone;
-//            this.priceFrom = priceFrom;
-//            this.priceTo = priceTo;
-//            this.m2From = m2From;
-//            this.m2To = m2To;
-//            this.sex = sex;
-//            this.searchSex = searchSex;
-//            this.fixedPrice = fixedPrice;
-//            this.separateRoom = separateRoom;
-//            this.categories = categories;
-//            this.municipalities = municipalties;
-//
+            this.description = description;
+            this.years = years;
+            this.searchYears = searchYears;
+            this.facebook = facebook;
+            this.phone = phone;
+            this.priceFrom = priceFrom;
+            this.priceTo = priceTo;
+            this.m2From = m2From;
+            this.m2To = m2To;
+            this.sex = sex;
+            this.searchSex = searchSex;
+            this.fixedPrice = fixedPrice;
+            this.separateRoom = separateRoom;
+            this.categories = categories;
+            this.municipalities = municipalties;
+            this.municipalityCode = municipalityCode;
         }
-
-
 
         @Override
         protected String doInBackground(Void... params) {
 
             String data = "";
-
+            Data d = new Data(getApplicationContext());
             try {
                 URL url = new URL("http://n3mesis-001-site1.htempurl.com/api/Roommates/Create"); //Enter URL here
                 HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setRequestMethod("POST"); // here you are telling that it is a POST request, which can be changed into "PUT", "GET", "DELETE" etc.
                 httpURLConnection.setRequestProperty("Content-Type", "application/json"); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
-                httpURLConnection.setRequestProperty("Content-Length", "133"); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
+//                httpURLConnection.setRequestProperty("Authorization", "bearer " + d.accessToken()); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
+
                 httpURLConnection.connect();
 
 
                 JSONObject jsonObject = new JSONObject();
+                JSONArray jsonArray = new JSONArray();
+                JSONObject JSONmunicipality = new JSONObject();
+
+                JSONmunicipality.put("Iso31662", municipalityCode);
+                JSONmunicipality.put("NameMkd", municipalities);
+
+                jsonArray.put(JSONmunicipality);
+
+
+
 //                jsonObject.put("UserId", "arg_1");
                 jsonObject.put("Title", title);
-                jsonObject.put("MeYears", 22);
-                jsonObject.put("SearchYears", 22);
-                jsonObject.put("Facebook", "facebook.com/cimer");
-                jsonObject.put("Phone", "099898765");
-                jsonObject.put("Description", "arg_1");
+                jsonObject.put("MeYears", years);
+                jsonObject.put("SearchYears", searchYears);
+                jsonObject.put("Facebook", facebook);
+                jsonObject.put("Phone", phone);
+                jsonObject.put("Description", description);
 //                jsonObject.put("Created", new Date().toString());
 //                jsonObject.put("ValidUntilDate", new Date().toString());
-                jsonObject.put("PriceFrom", 100);
-                jsonObject.put("PriceTo", 200);
-                jsonObject.put("M2From", 0);
-                jsonObject.put("M2To", 200);
-                jsonObject.put("SeparateRoom", true);
-                jsonObject.put("FixedPrice", false);
-//                jsonObject.put("Municipalities", "arg_1");
+                jsonObject.put("PriceFrom", priceFrom);
+                jsonObject.put("PriceTo", priceTo);
+                jsonObject.put("M2From", m2From);
+                jsonObject.put("M2To", m2To);
+                jsonObject.put("SeparateRoom", separateRoom);
+                jsonObject.put("FixedPrice", fixedPrice);
+                jsonObject.put("Municipalities", jsonArray);
 
                 Log.e("TAG JSON", jsonObject.toString());
 
